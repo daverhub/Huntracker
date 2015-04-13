9fe4b2ce354d0bb8f2bdb2629a36dbd87f0074b6
David Tsang <davidytsang@gmail.com>
2015-04-13 11:16:22 -0400

    updated Indeed controller

--- app/models/indeed.rb
+++ app/models/indeed.rb
@@ -1,16 +1,16 @@
-require 'httparty'
-module IndeedAPI
-  include HTTParty
-  base_uri 'http://api.indeed.com/ads/apisearch?'
-
-  def search
-      @client = Indeed::Client.new "YOUR_PUBLISHER_NUMBER"
-      @params = {
-          :q => 'ruby',
-          :l => 'austin',
-          :userip => '1.2.3.4',
-          :useragent => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)',
-      }
-  end
-
-end
+# require 'httparty'
+# module IndeedAPI
+#   include HTTParty
+#   base_uri 'http://api.indeed.com/ads/apisearch?'
+#
+#   def search
+#       @client = Indeed::Client.new "YOUR_PUBLISHER_NUMBER"
+#       @params = {
+#           :q => 'ruby',
+#           :l => 'austin',
+#           :userip => '1.2.3.4',
+#           :useragent => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)',
+#       }
+#   end
+#
+# end
