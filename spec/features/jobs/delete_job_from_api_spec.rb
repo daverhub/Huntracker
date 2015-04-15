46486100b099d7aaa55855c2f10c68bc49a43390
David Tsang <davidytsang@gmail.com>
2015-04-15 11:20:32 -0400

    testing job controller

---
+++ spec/features/jobs/delete_job_from_api_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature "Remove job from watch list" do
+  context 'as an authorized user' do
+    let(:user) { FactoryGirl.create(:user) }
+
+    before :each do
+      sign_in_as user
+    end
+
+    scenario 'successfully Remove job' do
+      job = FactoryGirl.create(:job)
+
+      visit job_path(job)
+      click_link "Delete"
+
+      expect(page).to_not have_link(job.title)
+    end
+  end
+end
